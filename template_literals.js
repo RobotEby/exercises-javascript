ba6bbe66087e5c9a48cd3809b81bf1fcf8600894
MrRobot <ubuntu.onion@hotmail.com>
2024-05-22 22:50:50 -0300

    template literals

--- template_literals.js
+++ template_literals.js
@@ -4,9 +4,9 @@ const Arquitetura = 'NVIDIA Ampere';
 const Tamanhodoprocesso = '8nm';
 const Transistores = '28,3 bilhões';
 const Tamanhodamatriz = '628,4 mm';
-const NúcleosCUDA = '10752';
-const Núcleostensores = '336';
-const NúcleosRT = '84';
+const NúcleosCUDA = 10752;
+const Núcleostensores = 336;
+const NúcleosRT = 84;
 
 let hardwares = 
 `Marca: ${Marca}
@@ -16,6 +16,7 @@ Tamanho do processo: ${Tamanhodoprocesso}
 Transistores: ${Transistores}
 Tamanho da matriz: ${Tamanhodamatriz}
 NúcleosCUDA: ${NúcleosCUDA}
+Núcleostensores: ${Núcleostensores}
 NúcleosRT: ${NúcleosRT}`;
 
 console.log(hardwares);
